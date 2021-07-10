00a8cad95581be80d2b8a40011a7bf8a2cbc248b
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-07-09 18:32:07 -0700

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -7,13 +7,27 @@ var firebaseConfig = {
       messagingSenderId: "491868464244",
       appId: "1:491868464244:web:2bcbd3b8a799c0c405dd21"
     };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+function addRoom() {
+room_name = document.getElementById("room_name").value;
+firebase.database().ref("/").child(room_name).update({
+purpose : "adding room name"
+});
 
+localStorage.setItem("room_name", room_name);
+
+window.location = "kwitter_page.html";
+}
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+      console.log("Room Name - " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
       //End code
       });});}
 getData();
+
+function redirectToRoomName(name) {
+console.log(name);
+localStorage.setItem("room_name", name);
+window.location = "kwitter_page.html";
+}

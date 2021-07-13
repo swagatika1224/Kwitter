b31c711ebc216a1bf41ebc758832b0b9e578e092
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-07-13 16:56:39 -0700

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -9,7 +9,11 @@ var firebaseConfig = {
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-
+    user_name = localStorage.getItem("user_name"); 
+    document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!"; 
+    function addRoom() { room_name = document.getElementById("room_name").value; 
+    firebase.database().ref("/").child(room_name).update({ purpose : "adding room name" }); 
+    localStorage.setItem("room_name", room_name); window.location = "kwitter_page.html"; }
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
@@ -32,3 +36,4 @@ localStorage.removeItem("user_name");
 localStorage.removeItem("room_name");
 window.location = "kwitter.html";
 }
+

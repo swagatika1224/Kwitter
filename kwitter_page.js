6c5846405a947a1c942b4c464cbef457a72e7470
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-07-15 14:54:27 -0700

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -9,7 +9,8 @@ var firebaseConfig = {
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-
+user_name = localStorage.getItem("user_name");
+room_name = localStorage.getItem("room_name");
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
          message_data = childData;
